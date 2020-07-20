8dfac0d5cee81e26e2091678eeb63636ef4eff8d
Melanie Anderson <melanieanderson1995@gmail.com>
2020-07-20 19:24:33 +0100

    Added helper to DRY up code

--- spec/features/check_hit_points_spec.rb
+++ spec/features/check_hit_points_spec.rb
@@ -2,10 +2,7 @@ require 'spec_helper'
 
 feature "Check hit points" do
   scenario "See Player 2's hit points" do
-    visit("/")
-    fill_in :player_1_name, with: "Lanie"
-    fill_in :player_2_name, with: "Ada"
-    click_button "Submit"
+    sign_in_and_play
     expect(page).to have_content "Ada: 50 HP"
   end
 end
